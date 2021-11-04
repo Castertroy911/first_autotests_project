2f8b50e06e24af096db21f4c585fb1cfa43867c7
d.leonov <d.leonov@m2e.team>
2021-11-04 20:11:50 +0200

    Add function that initialize and close browser after each test and login to magento

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from m2.config.local_config import *
+from Credentials.credentials_magento import *
+
+@pytest.fixture(scope='module')
+def browser():
+    with webdriver.Chrome() as driver:
+        driver.implicitly_wait(5)
+        driver.get(magento_base_url)
+        name = driver.find_element_by_id("username")
+        name.send_keys(user_name)
+        password = driver.find_element_by_id("login")
+        password.send_keys(user_password)
+        button = driver.find_element_by_class_name("action-login")
+        button.click()
+        yield driver
+
+
+
