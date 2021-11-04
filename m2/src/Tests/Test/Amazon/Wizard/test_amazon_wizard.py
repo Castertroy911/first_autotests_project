5424bf4bb51f4596ad1c0e3f7512ebfbb7e9a9cb
d.leonov <d.leonov@m2e.team>
2021-11-04 20:27:25 +0200

    Create file for automating amazon wizard

---
+++ m2/src/Tests/Test/Amazon/Wizard/test_amazon_wizard.py
@@ -0,0 +1,73 @@
+import time
+
+from login_amz import *
+from Credentials.credentials_amazon import *
+from selenium.webdriver.support.ui import Select
+from conftest import *
+from selenium.common.exceptions import TimeoutException
+
+
+@pytest.mark.wizard
+class TestWizard():
+    def  test_amazon_wizard(self, browser):
+        # Начинаем прохождение визарда Walmart
+        browser.get("http://10.0.30.122/magento_2/prefix_clear/admin/m2epro/wizard_installationAmazon/")
+        try:
+            close = wait(".action", browser)
+        except TimeoutException as err:
+            pass
+
+        try:
+            # Первый шаг визарда
+            email = browser.find_element(By.CSS_SELECTOR, "#form_email")
+            email.clear()
+            email.send_keys("admin@admin.com")
+
+            first_name = browser.find_element(By.CSS_SELECTOR, "#first_name")
+            first_name.clear()
+            first_name.send_keys("admin")
+
+            last_name = browser.find_element(By.CSS_SELECTOR, "#last_name")
+            last_name.clear()
+            last_name.send_keys("admin")
+
+            phone = browser.find_element(By.CSS_SELECTOR, "#phone")
+            phone.clear()
+            phone.send_keys("+1231234567")
+
+            country = Select(browser.find_element(By.ID, "country"))
+            country.select_by_visible_text("United States")
+
+            city = browser.find_element(By.CSS_SELECTOR, "#city")
+            city.clear()
+            city.send_keys("city")
+
+            postal_code = browser.find_element(By.CSS_SELECTOR, "#postal_code")
+            postal_code.clear()
+            postal_code.send_keys("90034")
+
+            agreement = wait("[value = '1']", browser)
+
+            next_step = wait("#continue", browser)
+        except NoSuchElementException as err:
+            pass
+
+        # Второй шаг визарда
+        marketplace = Select(browser.find_element(By.ID, "marketplace_id"))
+        marketplace.select_by_visible_text("United States")
+
+        button = wait("#continue", browser)
+
+        login = amazon_log_in(user_id_amazon, user_secret_amazon, two_step_auth, browser)
+
+        # Третий шаг визарда
+        asin = Select(browser.find_element(By.ID, "general_id"))
+        asin.select_by_visible_text("ASIN")
+
+        ean = Select(browser.find_element(By.ID, "worldwide_id"))
+        ean.select_by_visible_text("EAN")
+        time.sleep(1)
+
+        button = wait("#continue", browser)
+
+        button = wait("#skip", browser)
