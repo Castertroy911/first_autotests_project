b8ea84eb1ae7fa418463fba3bd781aa8406c3a0d
d.leonov <d.leonov@m2e.team>
2021-11-08 15:02:38 +0200

    Add some data for creating walmart policies

---
+++ m2/data/form/local/walmart/template/walmart_category_data.py
@@ -0,0 +1,6 @@
+from m2.config.local_config import *
+
+walmart_policy_url = "http://" + VM_IP + "/magento_2/prefix_clear/admin/m2epro/walmart_template/index/"
+category_title = "auto created category policy"
+marketplace_title = "United States"
+category_id = "284435000000"
