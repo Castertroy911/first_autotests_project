c8dfebb756c294ba557af492434c2b2d61c490b1
d.leonov <d.leonov@m2e.team>
2021-11-08 18:15:26 +0200

    Delete some variables to avoid extra using of memory

--- m2/src/Tests/Test/Ebay/3. Template/Create/test_return_policy.py
+++ m2/src/Tests/Test/Ebay/3. Template/Create/test_return_policy.py
@@ -11,9 +11,9 @@ class TestPolicy():
     def test_return_policy(self, browser):
         browser.get(ebay_policy_url)
 
-        return_policy = wait_element_clickable(".action-toggle", browser)
+        wait_element_clickable(".action-toggle", browser)
 
-        add = wait_presence_of_element("#add_policy-return", browser)
+        wait_presence_of_element("#add_policy-return", browser)
 
         title = browser.find_element(By.ID, "title")
         title.send_keys(return_title)
@@ -21,9 +21,8 @@ class TestPolicy():
         marketplace = Select(browser.find_element(By.ID, "marketplace_id"))
         marketplace.select_by_visible_text(return_marketplace)
 
-        button = wait_presence_of_element("#save_and_continue-button", browser)
+        wait_presence_of_element("#save_and_continue-button", browser)
 
-        # assert browser.find_element(By.CSS_SELECTOR, "[data-ui-id='messages-message-success']"), "Policy wasn't saved"
         # Проверяем, что все значения установлены по умолчанию
         is_element_selected("#return_accepted>[value='ReturnsAccepted']",\
                             "Something wrong with Return Policy", browser)
@@ -43,7 +42,7 @@ class TestPolicy():
 
         # Выбираем функцию No Returns Accepted
         select_by_value("#return_accepted", "ReturnsNotAccepted", browser)
-        button = wait_presence_of_element("#save_and_continue-button", browser)
+        wait_presence_of_element("#save_and_continue-button", browser)
         try:
             wait_element_clickable("#do_not_show_again", browser)
             time.sleep(0.1)
@@ -63,7 +62,7 @@ class TestPolicy():
         # Выбираем Returns accepted для Domestic and International returns
         select_by_value("#return_accepted", "ReturnsAccepted", browser)
         select_by_value("#return_international_accepted", "ReturnsAccepted", browser)
-        button = wait_element_clickable("#save_and_continue-button", browser)
+        wait_element_clickable("#save_and_continue-button", browser)
 
         # Проверяем, что изменения сохранились
         is_element_selected("#return_accepted>[value='ReturnsAccepted']", "Changes not saved", browser)
@@ -76,7 +75,7 @@ class TestPolicy():
         select_by_value("#return_international_option", "MoneyBackOrReplacement", browser)
         select_by_value("#return_international_within", "Days_30", browser)
         select_by_value("#return_international_shipping_cost", "Seller", browser)
-        button = wait_element_clickable("#save_and_continue-button", browser)
+        wait_element_clickable("#save_and_continue-button", browser)
 
         is_element_selected("#return_option>[value='MoneyBackOrReplacement']", "Changes not saved", browser)
         is_element_selected("#return_within>[value='Days_30']", "Changes not saved", browser)
