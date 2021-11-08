bf5101d075ae3eab9d5df015079a821d05db0b6b
d.leonov <d.leonov@m2e.team>
2021-11-08 15:18:14 +0200

    Rename wait function and add one more new function wait_element_clickable

--- waitings.py
+++ waitings.py
@@ -3,7 +3,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 
-def wait(a, browser):
-    clickable_element = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, a)))
+def wait_presence_of_element(selector, browser):
+    located_element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
+    located_element.click()
+
+
+def wait_element_clickable(selector, browser):
+    clickable_element = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, selector)))
     clickable_element.click()
