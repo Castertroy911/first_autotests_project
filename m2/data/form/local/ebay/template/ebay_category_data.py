edc32bf07b46cd471cd627f429d0c93ae58aad08
d.leonov <d.leonov@m2e.team>
2021-11-08 15:02:30 +0200

    Add some data for creating ebay policies

---
+++ m2/data/form/local/ebay/template/ebay_category_data.py
@@ -0,0 +1,11 @@
+from m2.config.local_config import *
+
+ebay_policy_url = "http://" + VM_IP + "/magento_2/prefix_clear/admin/m2epro/ebay_template/index/"
+# Payment Policy
+payment_title = "Auto Created Payment Policy"
+payment_marketplace = "United States"
+payment_mail = "admin@admin.com"
+
+# Return Policy
+return_title = "Auto Created Return Policy"
+return_marketplace = "United States"
