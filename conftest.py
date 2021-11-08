5c60bc54257dfc8d84a4c0facb2cc29e6f4c14f8
d.leonov <d.leonov@m2e.team>
2021-11-08 12:41:58 +0200

    Delete deprecated commands _by_

--- conftest.py
+++ conftest.py
@@ -3,18 +3,19 @@ from selenium import webdriver
 from m2.config.local_config import *
 from Credentials.credentials_magento import *
 import time
+from selenium.webdriver.common.by import By
 
 
 @pytest.fixture(scope='module')
-def browser():
+def browser(self):
     with webdriver.Chrome() as driver:
         driver.implicitly_wait(5)
         driver.get(magento_base_url)
-        name = driver.find_element_by_id("username")
+        name = driver.find_element(By.ID, "username")
         name.send_keys(user_name)
-        password = driver.find_element_by_id("login")
+        password = driver.find_element(By. ID, "login")
         password.send_keys(user_password)
-        button = driver.find_element_by_class_name("action-login")
+        button = driver.find_element(By.CLASS_NAME, "action-login")
         button.click()
         yield driver
 
